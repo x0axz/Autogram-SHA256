80ba17ff0aafdb8a1e469355673e9d65275ba62e
Nooh <x0axz@protonmail.com>
2023-10-04 22:10:34 +0500

    Seperate file for loop & permutation method

--- Python/autogram-sha256-permutation.py
+++ Python/autogram-sha256-permutation.py
@@ -44,35 +44,6 @@ def Autogram_SHA256SUM():
                 print(sentence)
                 print(sentence_hash)
 
-    '''
-    alphabets = {"a","b","c","d","e","f"}
-    numbers_words = {"zero","one","two","three","four","five","six","seven","eight","nine"}
-
-    for a1 in alphabets:
-        for a2 in alphabets:
-            for n1 in numbers_words:
-                for n2 in numbers_words:
-                    for n3 in numbers_words:
-                        for n4 in numbers_words:
-                            for n5 in numbers_words:
-                                
-                                autogram = numbers_dict[n1] + numbers_dict[n2] + numbers_dict[n3] + a1 + numbers_dict[n4] + a2 + numbers_dict[n5]
-
-                                sentence = f"The SHA256 for this sentence begins with: {n1}, {n2}, {n3}, {a1}, {n4}, {a2} and {n5}."
-                                
-                                # Encode the sentence as bytes using UTF-8 encoding
-                                sentence_encoded = sentence.encode('utf-8')
-
-                                # Create a SHA-256 hash object
-                                hash_value = hashlib.sha256(sentence_encoded)
-
-                                # Get the hexadecimal representation of the hash
-                                sentence_hash = hash_value.hexdigest()
-                                
-                                if (autogram == sentence_hash[:7]):
-                                    print(sentence)
-                                    print(sentence_hash)
-    '''
 
 if __name__ == '__main__':
     Autogram_SHA256SUM()
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ On Hacker News, there was a [post](https://news.ycombinator.com/item?id=19003644
 
 As a result, I developed scripts in [Python](https://github.com/x0axz/Autogram-SHA256/blob/main/Python/autogram-sha256.py), [Go](https://github.com/x0axz/Autogram-SHA256/blob/main/Go/autogram-sha256.go), and [C#](https://github.com/x0axz/Autogram-SHA256/blob/main/C%23/autogram-sha256.cs) to discover additional SHA256 hashes with this characteristic.
 
-If you stumble upon any more captivating discoveries while tinkering with the code, please don't hesitate to implement them and submit a pull request.
+If you stumble upon any more captivating discoveries while tinkering with the code, please implement them and share them by submitting a pull request.
 
 ## Example
 
